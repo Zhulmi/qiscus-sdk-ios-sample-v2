ce32f600f8847fc1f716a16e32982aa1ba6089ed
ariefnurputranto <arief@qiscus.co>
2018-10-10 12:47:03 +0700

    add file custom cell

---
+++ qiscus-sdk-ios-sample-v2/Views/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  qiscus-sdk-ios-sample-v2
+//
+//  Created by asharijuang on 10/10/18.
+//  Copyright © 2018 Qiscus Technology. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
